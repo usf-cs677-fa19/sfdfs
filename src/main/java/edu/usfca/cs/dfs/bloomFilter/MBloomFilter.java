b37f531fc9e9fde3f1f64c78a9a2890af2ee3152
Manali Patil <mmpatil@dons.usfca.edu>
2019-09-22 22:22:48 -0700

    Creating file MBloomfilter

---
+++ src/main/java/edu/usfca/cs/dfs/bloomFilter/MBloomFilter.java
@@ -0,0 +1,13 @@
+package edu.usfca.cs.dfs.bloomFilter;
+
+import java.util.HashMap;
+
+public class MBloomFilter {
+
+    private HashMap<Integer,Integer> bloomfilter;
+
+    private int size;
+
+
+
+}
