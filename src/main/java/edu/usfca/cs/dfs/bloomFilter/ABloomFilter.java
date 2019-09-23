25d7a6a1a54922da82ad0f73f3d28a81dd03b5e5
Anurag Jha <ajha6@dons.usfca.edu>
2019-09-22 22:26:51 -0700

    Creating ABloomFilter class

--- src/main/java/edu/usfca/cs/dfs/bloomFilter/ABloomFilter.java
+++ src/main/java/edu/usfca/cs/dfs/bloomFilter/ABloomFilter.java
@@ -1,4 +1,18 @@
 package edu.usfca.cs.dfs.bloomFilter;
 
-public class ABloomFilter {
+public class ABloomFilter implements Filter {
+    @Override
+    public int add(String str) {
+        return 0;
+    }
+
+    @Override
+    public boolean check(String str) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String str) {
+        return false;
+    }
 }
